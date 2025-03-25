f9940a536c182094f7acffc2ee174a1b811571fb
Fede Diaz <federico.diaz3100@Macbooks-MacBook-Air.local>
2025-03-25 10:34:51 -0300

    Subiendo a PythonEverywhere

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-13^#g1!m1#ewxm7)=)6qj00vm72=054ke$j&q4ja&ld2ip#!lc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['portfolio-kblg.onrender.com','127.0.0.1',]
+ALLOWED_HOSTS = ['portfolio-kblg.onrender.com','127.0.0.1','FedeDiaz.pythonanywhere.com',]
 
 
 # Application definition
